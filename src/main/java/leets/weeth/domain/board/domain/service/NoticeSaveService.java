99cba65a6514962e6bb7b32ef097d11058d5d261
hyxklee <ewgt1234@naver.com>
2024-08-02 20:48:18 +0900

    feat: 공지사항 save 서비스 구현

---
+++ src/main/java/leets/weeth/domain/board/domain/service/NoticeSaveService.java
@@ -0,0 +1,17 @@
+package leets.weeth.domain.board.domain.service;
+
+import leets.weeth.domain.board.domain.entity.Notice;
+import leets.weeth.domain.board.domain.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeSaveService {
+
+    private final NoticeRepository noticeRepository;
+
+    public void save(Notice notice){
+        noticeRepository.save(notice);
+    }
+}
