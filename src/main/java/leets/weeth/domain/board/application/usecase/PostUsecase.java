1cde8a1e03683c3b1b5e01df8929cdab9d0567d0
hyxklee <ewgt1234@naver.com>
2024-08-02 21:33:08 +0900

    feat: 게시글 저장 유즈케이스 구현

--- src/main/java/leets/weeth/domain/board/application/usecase/PostUsecase.java
+++ src/main/java/leets/weeth/domain/board/application/usecase/PostUsecase.java
@@ -1,9 +1,8 @@
 package leets.weeth.domain.board.application.usecase;
 
 import leets.weeth.domain.board.application.dto.PostDTO;
-import leets.weeth.domain.user.domain.entity.User;
 
 public interface PostUsecase {
 
-    void save(PostDTO.Save request, User user);
+    void save(PostDTO.Save request, Long userId);
 }
