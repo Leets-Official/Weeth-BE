d6a54d47ea2d11aefaa2e5e9b50e9b1090b63c50
hyxklee <ewgt1234@naver.com>
2024-08-02 20:47:58 +0900

    feat: 공지사항 mapper 구현

---
+++ src/main/java/leets/weeth/domain/board/application/mapper/NoticeMapper.java
@@ -0,0 +1,14 @@
+package leets.weeth.domain.board.application.mapper;
+
+import leets.weeth.domain.board.application.dto.NoticeDTO;
+import leets.weeth.domain.board.domain.entity.Notice;
+import leets.weeth.domain.user.domain.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface NoticeMapper {
+
+    Notice from(NoticeDTO.Save dto, User user);
+}
