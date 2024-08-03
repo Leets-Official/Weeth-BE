bbb1162e8c564a54ba89cf8ff5aa9c6b77c1d6e8
hyxklee <ewgt1234@naver.com>
2024-08-03 20:23:59 +0900

    feat: comment mapper 구현

---
+++ src/main/java/leets/weeth/domain/comment/application/mapper/CommentMapper.java
@@ -0,0 +1,33 @@
+package leets.weeth.domain.comment.application.mapper;
+
+import leets.weeth.domain.board.domain.entity.Post;
+import leets.weeth.domain.comment.application.dto.CommentDTO;
+import leets.weeth.domain.comment.domain.entity.Comment;
+import leets.weeth.domain.user.domain.entity.User;
+import org.mapstruct.*;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CommentMapper {
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "content", source = "dto.content"),
+            @Mapping(target = "user", source = "user"),
+            @Mapping(target = "post", source = "post"),
+            @Mapping(target = "parent", source = "parent"),
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "modifiedAt", ignore = true),
+            @Mapping(target = "isDeleted", expression =  "java(false)")
+    })
+    Comment from(CommentDTO.Save dto, Post post, User user, Comment parent);
+
+    @Mapping(target = "name", source = "user.name")
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "time", source = "modifiedAt")
+    CommentDTO.Response to(Comment comment);
+
+    @Mapping(target = "id", source = "commentId")
+    @Mapping(target = "user", source = "user")
+    Comment update(Long commentId, CommentDTO.Update dto, User user);
+
+}
