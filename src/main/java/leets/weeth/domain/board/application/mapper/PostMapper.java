733e3498ac6d032e9b80c3140e45dd6f5f4ec014
hyxklee <ewgt1234@naver.com>
2024-08-03 20:25:15 +0900

    feat: postMapper 매핑 추가

--- src/main/java/leets/weeth/domain/board/application/mapper/PostMapper.java
+++ src/main/java/leets/weeth/domain/board/application/mapper/PostMapper.java
@@ -3,13 +3,14 @@ package leets.weeth.domain.board.application.mapper;
 import leets.weeth.domain.board.application.dto.BoardDTO;
 import leets.weeth.domain.board.application.dto.PostDTO;
 import leets.weeth.domain.board.domain.entity.Post;
+import leets.weeth.domain.comment.application.mapper.CommentMapper;
 import leets.weeth.domain.user.domain.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = CommentMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PostMapper {
 
     @Mapping(target = "id", ignore = true)
@@ -20,6 +21,7 @@ public interface PostMapper {
     @Mapping(target = "user", source = "user")
     Post update(Long postId, PostDTO.Update dto, User user);
 
+    @Mapping(target = "name", source = "user.name")
+    @Mapping(target = "comments", source = "comments")
     BoardDTO.Response to(Post post);
-
 }
