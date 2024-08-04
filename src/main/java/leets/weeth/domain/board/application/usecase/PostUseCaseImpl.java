a04128ab14c8b6eb2b23d4108db33a9232a64146
hyxklee <ewgt1234@naver.com>
2024-08-04 12:21:15 +0900

    refactor: 엔티티 수정

--- src/main/java/leets/weeth/domain/board/application/usecase/PostUseCaseImpl.java
+++ src/main/java/leets/weeth/domain/board/application/usecase/PostUseCaseImpl.java
@@ -2,18 +2,20 @@ package leets.weeth.domain.board.application.usecase;
 
 import leets.weeth.domain.board.application.dto.PostDTO;
 import leets.weeth.domain.board.application.mapper.PostMapper;
-import leets.weeth.domain.board.domain.entity.Notice;
 import leets.weeth.domain.board.domain.entity.Post;
 import leets.weeth.domain.board.domain.service.PostDeleteService;
 import leets.weeth.domain.board.domain.service.PostFindService;
 import leets.weeth.domain.board.domain.service.PostSaveService;
 import leets.weeth.domain.board.domain.service.PostUpdateService;
+import leets.weeth.domain.file.service.FileSaveService;
 import leets.weeth.domain.user.domain.entity.User;
 import leets.weeth.domain.user.domain.service.UserGetService;
-import leets.weeth.global.common.error.exception.custom.UserNotFoundException;
 import leets.weeth.global.common.error.exception.custom.UserNotMatchException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,18 +26,31 @@ public class PostUseCaseImpl implements PostUsecase{
     private final PostUpdateService postUpdateService;
     private final PostDeleteService postDeleteService;
     private final UserGetService userGetService;
+    private final FileSaveService fileSaveService;
     private final PostMapper mapper;
 
     @Override
-    public void save(PostDTO.Save request, Long userId) {
+    public void save(PostDTO.Save request, List<MultipartFile> files, Long userId) {
         User user = userGetService.find(userId);
-        postSaveService.save(mapper.from(request, user));
+
+        List<String> fileUrls;
+        fileUrls = fileSaveService.uploadFiles(files);
+        postSaveService.save(mapper.from(request, fileUrls, user));
     }
 
+    /*
+    게시글 수정 시 파일이 넘어오지 않으면, 파일이 제거됨.
+    수정할 때도 파일을 같이 넘기면, 또 업로드가 발생함 -> 근데 동일한 파일은 재 업로드가 아닌거 같기도 함.
+    -> 테스트 해보기
+     */
     @Override
-    public void update(Long postId, PostDTO.Update dto, Long userId) throws UserNotMatchException {
-        User user = validateOwner(postId, userId);
-        postUpdateService.update(postId, dto, user);
+    public void update(Long postId, PostDTO.Update dto, List<MultipartFile> files, Long userId) throws UserNotMatchException {
+        Post post = validateOwner(postId, userId);
+
+        List<String> fileUrls;
+        fileUrls = fileSaveService.uploadFiles(files);
+
+        postUpdateService.update(post, dto, fileUrls);
     }
 
     @Override
@@ -44,14 +59,13 @@ public class PostUseCaseImpl implements PostUsecase{
         postDeleteService.delete(postId);
     }
 
-    private User validateOwner(Long postId, Long userId) throws UserNotMatchException {
+    private Post validateOwner(Long postId, Long userId) throws UserNotMatchException {
         Post post = postFindService.find(postId);
-        User user = userGetService.find(userId);
 
-        if (!post.getUser().equals(user)) {
+        if (!post.getUser().getId().equals(userId)) {
             throw new UserNotMatchException();
         }
-        return user;
+        return post;
     }
 
 }
