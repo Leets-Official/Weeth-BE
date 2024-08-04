43cdc77ded6e167da3685595744b957016cd6f26
hyxklee <ewgt1234@naver.com>
2024-08-04 12:24:23 +0900

    feat: 공지사항 매퍼 로직 상세히 구현

--- src/main/java/leets/weeth/domain/board/application/mapper/NoticeMapper.java
+++ src/main/java/leets/weeth/domain/board/application/mapper/NoticeMapper.java
@@ -3,21 +3,29 @@ package leets.weeth.domain.board.application.mapper;
 import leets.weeth.domain.board.application.dto.BoardDTO;
 import leets.weeth.domain.board.application.dto.NoticeDTO;
 import leets.weeth.domain.board.domain.entity.Notice;
+import leets.weeth.domain.comment.application.mapper.CommentMapper;
 import leets.weeth.domain.user.domain.entity.User;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = CommentMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface NoticeMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "user", source = "user")
-    Notice from(NoticeDTO.Save dto, User user);
+    Notice from(NoticeDTO.Save dto, List<String> fileUrls, User user);
 
-    @Mapping(target = "id", source = "noticeId")
-    @Mapping(target = "user", source = "user")
-    Notice update(Long noticeId, NoticeDTO.Update dto, User user);
+//    @Mapping(target = "id", source = "noticeId")
+//    @Mapping(target = "user", source = "user")
+//    Notice update(Long noticeId, NoticeDTO.Update dto, List<String> fileUrls, User user);
 
-
+    @Mapping(target = "name", source = "user.name")
+    @Mapping(target = "comments", source = "comments")
+    @Mapping(target = "time", source = "modifiedAt")
     BoardDTO.Response to(Notice notice);
 
 }
