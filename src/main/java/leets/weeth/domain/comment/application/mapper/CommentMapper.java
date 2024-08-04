7b8d255f491e23e8ab745e9da3bdad3343386266
hyxklee <ewgt1234@naver.com>
2024-08-04 12:26:06 +0900

    feat: 커멘트 매퍼 로직 상세히 구현

--- src/main/java/leets/weeth/domain/comment/application/mapper/CommentMapper.java
+++ src/main/java/leets/weeth/domain/comment/application/mapper/CommentMapper.java
@@ -1,5 +1,6 @@
 package leets.weeth.domain.comment.application.mapper;
 
+import leets.weeth.domain.board.domain.entity.Notice;
 import leets.weeth.domain.board.domain.entity.Post;
 import leets.weeth.domain.comment.application.dto.CommentDTO;
 import leets.weeth.domain.comment.domain.entity.Comment;
@@ -17,17 +18,30 @@ public interface CommentMapper {
             @Mapping(target = "parent", source = "parent"),
             @Mapping(target = "createdAt", ignore = true),
             @Mapping(target = "modifiedAt", ignore = true),
+            @Mapping(target = "notice", ignore = true),
             @Mapping(target = "isDeleted", expression =  "java(false)")
     })
     Comment from(CommentDTO.Save dto, Post post, User user, Comment parent);
 
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "content", source = "dto.content"),
+            @Mapping(target = "user", source = "user"),
+            @Mapping(target = "notice", source = "notice"),
+            @Mapping(target = "parent", source = "parent"),
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "modifiedAt", ignore = true),
+            @Mapping(target = "post", ignore = true),
+            @Mapping(target = "isDeleted", expression =  "java(false)")
+    })
+    Comment from(CommentDTO.Save dto, Notice notice, User user, Comment parent);
+
     @Mapping(target = "name", source = "user.name")
-    @Mapping(target = "userId", source = "user.id")
     @Mapping(target = "time", source = "modifiedAt")
     CommentDTO.Response to(Comment comment);
 
-    @Mapping(target = "id", source = "commentId")
-    @Mapping(target = "user", source = "user")
-    Comment update(Long commentId, CommentDTO.Update dto, User user);
+//    @Mapping(target = "id", source = "commentId")
+//    @Mapping(target = "user", source = "user")
+//    Comment update(Long commentId, CommentDTO.Update dto, User user);
 
 }
