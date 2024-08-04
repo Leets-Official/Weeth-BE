9092c9bc8ee758cdc5a4fb3864a0825e0e11caad
hyxklee <ewgt1234@naver.com>
2024-08-04 12:27:15 +0900

    feat: 게시판 조회 api 매핑

--- src/main/java/leets/weeth/domain/board/presentation/BoardController.java
+++ src/main/java/leets/weeth/domain/board/presentation/BoardController.java
@@ -5,6 +5,7 @@ import leets.weeth.domain.board.application.usecase.BoardUseCase;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +23,19 @@ public class BoardController {
         return CommonResponse.createSuccess(boardUseCase.findNotices());
     }
 
+    @GetMapping("/notices/{noticeId}")
+    public CommonResponse<BoardDTO.Response> findNoticeById(@PathVariable Long noticeId) {
+        return CommonResponse.createSuccess(boardUseCase.findNotice(noticeId));
+    }
+
     @GetMapping("/posts")
     public CommonResponse<List<BoardDTO.Response>> findPosts() {
         return CommonResponse.createSuccess(boardUseCase.findPosts());
+    }
+
+    @GetMapping("/posts/{postId}")
+    public CommonResponse<BoardDTO.Response> findPost(@PathVariable Long postId) {
+        return CommonResponse.createSuccess(boardUseCase.findPost(postId));
     }
 
 }
