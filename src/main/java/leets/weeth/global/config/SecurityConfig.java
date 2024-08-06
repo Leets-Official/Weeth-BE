e733d3363813ca9e810e3ab835ebac429f44ed17
koungq <guu119@naver.com>
2024-08-06 18:36:23 +0900

    Hotfix: SecurityConfig 경로 재설정

--- src/main/java/leets/weeth/global/config/SecurityConfig.java
+++ src/main/java/leets/weeth/global/config/SecurityConfig.java
@@ -64,13 +64,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         authorize ->
                                 authorize
-                                        .requestMatchers("/users/apply", "/users/duplication/**").permitAll()
+                                        .requestMatchers("/api/v1/users/apply", "/api/v1/users/email").permitAll()
                                         .requestMatchers("/health-check").permitAll()
                                         .requestMatchers("/adminpage/login","/adminpage/account", "/adminpage/attendance", "/adminpage/member","/adminpage/penalty",
                                                 "/css/**", "/js/**", "/img/**", "/scss/**", "/vendor/**").permitAll()
                                         // 스웨거 경로
                                         .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger/**").permitAll()
-                                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                                        .requestMatchers("/api/vi/admin/**").hasRole("ADMIN")
                                         .anyRequest().permitAll()
                 )
 
