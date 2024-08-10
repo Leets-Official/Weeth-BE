e84dcd54b59d0dfc542fa5866fee3a693a1c0f3e
hyxklee <ewgt1234@naver.com>
2024-08-10 22:53:39 +0900

    refactor: 글씨 크기 조정

--- src/main/resources/static/js/meeting.js
+++ src/main/resources/static/js/meeting.js
@@ -66,7 +66,7 @@ function displayMeetings(meetingArray) {
 
             const header = document.createElement('div');
             header.className = 'card-header py-3';
-            header.innerHTML = `<h4 class="m-0 font-weight-bold text-success">${cardinal}기 정기모임</h4>`;
+            header.innerHTML = `<h6 class="m-0 font-weight-bold text-success">${cardinal}기 정기모임</h6>`;
             section.appendChild(header);
 
             groupedMeetings[cardinal].forEach((meeting, index) => {
