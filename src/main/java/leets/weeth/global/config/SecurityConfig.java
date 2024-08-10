3a459d5aefb4541a5ee38b43b626904306664156
hyxklee <ewgt1234@naver.com>
2024-08-10 22:54:52 +0900

    feat: admin 기본 경로 추가

--- src/main/java/leets/weeth/global/config/SecurityConfig.java
+++ src/main/java/leets/weeth/global/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
                                 authorize
                                         .requestMatchers("/api/v1/users/apply", "/api/v1/users/email").permitAll()
                                         .requestMatchers("/health-check").permitAll()
-                                        .requestMatchers("/admin/login","/admin/account", "/admin/meeting", "/admin/member","/admin/penalty",
+                                        .requestMatchers("/admin","/admin/login","/admin/account", "/admin/meeting", "/admin/member","/admin/penalty",
                                                 "/css/**", "/js/**", "/img/**", "/scss/**", "/vendor/**").permitAll()
                                         // 스웨거 경로
                                         .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger/**").permitAll()
