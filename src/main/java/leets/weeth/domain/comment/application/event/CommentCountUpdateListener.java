44ead5616b0d47e47028d1ff5f3dac0fca4759db
hyxklee <ewgt1234@naver.com>
2024-08-18 20:33:07 +0900

    feat: 댓글 삭제 이벤트 발생 시 현재 댓글 수를 다시 계산하는 이벤트 리스너 생성

---
+++ src/main/java/leets/weeth/domain/comment/application/event/CommentCountUpdateListener.java
@@ -0,0 +1,35 @@
+package leets.weeth.domain.comment.application.event;
+
+import jakarta.persistence.EntityManager;
+import leets.weeth.domain.board.domain.entity.Notice;
+import leets.weeth.domain.board.domain.entity.Post;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CommentCountUpdateListener {
+    private final EntityManager entityManager;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handleNoticeCommentDeleted(NoticeCommentCountUpdate event) {
+        Notice notice = entityManager.merge(event.getNotice());
+        notice.updateCommentCount();
+
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handlePostCommentDeleted(PostCommentCountUpdate event) {
+        Post post = entityManager.merge(event.getPost());
+        post.updateCommentCount();
+    }
+
+}
