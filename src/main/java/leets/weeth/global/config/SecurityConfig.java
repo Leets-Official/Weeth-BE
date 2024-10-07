04a07371af23623f4ea4df3d9966b76ba95eb47b
hyxklee <ewgt1234@naver.com>
2024-10-07 17:40:20 +0900

    HOTFIX: cors 설정 재변경

--- src/main/java/leets/weeth/global/config/SecurityConfig.java
+++ src/main/java/leets/weeth/global/config/SecurityConfig.java
@@ -94,7 +94,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization, Authorization_refresh"));
