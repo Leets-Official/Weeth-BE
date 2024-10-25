91a84e9b5b0aaa4e6c145eb27d9131cf3d3c8acb
hyxklee <ewgt1234@naver.com>
2024-10-25 14:48:50 +0900

    feat: 스웨거에서도 refresh 헤더 테스트가 가능하게 구현

--- src/main/java/leets/weeth/global/config/swagger/SwaggerConfig.java
+++ src/main/java/leets/weeth/global/config/swagger/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,24 +21,47 @@ import java.util.List;
                 version = "v1.0.0"
         )
 )
+
 public class SwaggerConfig {
+
+    @Value("${weeth.jwt.access.header}")
+    private String accessHeader;
+
+    @Value("${weeth.jwt.refresh.header}")
+    private String refreshHeader; // 리프레시 토큰 헤더 이름
+
     @Bean
     public OpenAPI openAPI() {
-        SecurityScheme securityScheme = getSecurityScheme();
-        SecurityRequirement securityRequirement = getSecurityRequireMent();
+        SecurityScheme accessSecurityScheme = getAccessSecurityScheme();
+        SecurityScheme refreshSecurityScheme = getRefreshSecurityScheme();
 
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(List.of(securityRequirement));
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", accessSecurityScheme)
+                        .addSecuritySchemes("refreshBearerAuth", refreshSecurityScheme))
+                .security(List.of(
+                        new SecurityRequirement().addList("bearerAuth"),
+                        new SecurityRequirement().addList("refreshBearerAuth")
+                ));
     }
 
-    private SecurityScheme getSecurityScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER).name("Authorization");
+    private SecurityScheme getAccessSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(accessHeader);
     }
 
-    private SecurityRequirement getSecurityRequireMent() {
-        return new SecurityRequirement().addList("bearerAuth");
+    private SecurityScheme getRefreshSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(refreshHeader);
     }
+
 }
