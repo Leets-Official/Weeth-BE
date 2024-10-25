b9ed902601adf281da743e2a4b6ac7045eda792f
huncozyboy <leejh5838@naver.com>
2024-10-26 02:32:24 +0900

    refactor: NoticeController 관련 성공 응답 수정

--- src/main/java/leets/weeth/domain/board/presentation/NoticeController.java
+++ src/main/java/leets/weeth/domain/board/presentation/NoticeController.java
@@ -1,6 +1,9 @@
 package leets.weeth.domain.board.presentation;
 
 
+import static leets.weeth.domain.board.presentation.ResponseMessage.NOTICE_FIND_ALL_SUCCESS;
+import static leets.weeth.domain.board.presentation.ResponseMessage.NOTICE_FIND_BY_ID_SUCCESS;
+
 import leets.weeth.domain.board.application.dto.NoticeDTO;
 import leets.weeth.domain.board.application.usecase.NoticeUsecase;
 import leets.weeth.global.common.response.CommonResponse;
@@ -18,11 +21,11 @@ public class NoticeController {
 
     @GetMapping
     public CommonResponse<List<NoticeDTO.ResponseAll>> findNotices(@RequestParam(required = false) Long noticeId, @RequestParam Integer count) {
-        return CommonResponse.createSuccess(noticeUsecase.findNotices(noticeId, count));
+        return CommonResponse.createSuccess(NOTICE_FIND_ALL_SUCCESS.getMessage(), noticeUsecase.findNotices(noticeId, count));
     }
 
     @GetMapping("/{noticeId}")
     public CommonResponse<NoticeDTO.Response> findNoticeById(@PathVariable Long noticeId) {
-        return CommonResponse.createSuccess(noticeUsecase.findNotice(noticeId));
+        return CommonResponse.createSuccess(NOTICE_FIND_BY_ID_SUCCESS.getMessage(), noticeUsecase.findNotice(noticeId));
     }
 }
