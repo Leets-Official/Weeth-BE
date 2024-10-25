0ea901e7fcf3c7628bf899e4c44aab2dd7238164
huncozyboy <leejh5838@naver.com>
2024-10-26 02:21:32 +0900

    refactor: PostCommentController 관련 성공 응답 수정

--- src/main/java/leets/weeth/domain/comment/presentation/PostCommentController.java
+++ src/main/java/leets/weeth/domain/comment/presentation/PostCommentController.java
@@ -1,5 +1,9 @@
 package leets.weeth.domain.comment.presentation;
 
+import static leets.weeth.domain.comment.presentation.ResponseMessage.POST_COMMENT_CREATED_SUCCESS;
+import static leets.weeth.domain.comment.presentation.ResponseMessage.POST_COMMENT_DELETED_SUCCESS;
+import static leets.weeth.domain.comment.presentation.ResponseMessage.POST_COMMENT_UPDATED_SUCCESS;
+
 import jakarta.validation.Valid;
 import leets.weeth.domain.comment.application.dto.CommentDTO;
 import leets.weeth.domain.comment.application.usecase.PostCommentUsecase;
@@ -8,8 +12,6 @@ import leets.weeth.domain.user.application.exception.UserNotMatchException;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import static leets.weeth.domain.comment.domain.entity.enums.ResponseMessage.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,19 +23,19 @@ public class PostCommentController {
     @PostMapping
     public CommonResponse<String> savePostComment(@RequestBody @Valid CommentDTO.Save dto, @PathVariable Long postId, @CurrentUser Long userId) {
         postCommentUsecase.savePostComment(dto, postId, userId);
-        return CommonResponse.createSuccess(COMMENT_CREATED_SUCCESS.getMessage());
+        return CommonResponse.createSuccess(POST_COMMENT_CREATED_SUCCESS.getMessage());
     }
 
     @PatchMapping("/{commentId}")
     public CommonResponse<String> updatePostComment(@RequestBody @Valid CommentDTO.Update dto, @PathVariable Long postId, @PathVariable Long commentId, @CurrentUser Long userId) throws UserNotMatchException {
         postCommentUsecase.updatePostComment(dto, postId, commentId, userId);
-        return CommonResponse.createSuccess(COMMENT_UPDATED_SUCCESS.getMessage());
+        return CommonResponse.createSuccess(POST_COMMENT_UPDATED_SUCCESS.getMessage());
     }
 
     @DeleteMapping("{commentId}")
     public CommonResponse<String> deletePostComment(@PathVariable Long commentId, @CurrentUser Long userId) throws UserNotMatchException {
         postCommentUsecase.deletePostComment(commentId, userId);
-        return CommonResponse.createSuccess(COMMENT_DELETED_SUCCESS.getMessage());
+        return CommonResponse.createSuccess(POST_COMMENT_DELETED_SUCCESS.getMessage());
     }
 
 }
