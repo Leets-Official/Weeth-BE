854653ddb08d9ae27f517baef7860809b4a4fae7
huncozyboy <leejh5838@naver.com>
2024-10-25 23:59:07 +0900

    refactor: EventAdminController 관련 성공 응답 수정

--- src/main/java/leets/weeth/domain/schedule/presentation/EventAdminController.java
+++ src/main/java/leets/weeth/domain/schedule/presentation/EventAdminController.java
@@ -20,18 +20,21 @@ public class EventAdminController {
     @PostMapping
     public CommonResponse<Void> save(@Valid @RequestBody Save dto, @CurrentUser Long userId) {
         eventUseCase.save(dto, userId);
-        return CommonResponse.createSuccess();
+        return CommonResponse.createSuccess(ResponseMessage.EVENT_SAVE_SUCCESS.getStatusCode(),
+                ResponseMessage.EVENT_SAVE_SUCCESS.getMessage());
     }
 
     @PatchMapping("/{eventId}")
     public CommonResponse<Void> update(@PathVariable Long eventId, @Valid @RequestBody EventDTO.Update dto, @CurrentUser Long userId) {
         eventUseCase.update(eventId, dto, userId);
-        return CommonResponse.createSuccess();
+        return CommonResponse.createSuccess(ResponseMessage.EVENT_UPDATE_SUCCESS.getStatusCode(),
+                ResponseMessage.EVENT_UPDATE_SUCCESS.getMessage());
     }
 
     @DeleteMapping("/{eventId}")
     public CommonResponse<Void> delete(@PathVariable Long eventId) {
         eventUseCase.delete(eventId);
-        return CommonResponse.createSuccess();
+        return CommonResponse.createSuccess(ResponseMessage.EVENT_DELETE_SUCCESS.getStatusCode(),
+                ResponseMessage.EVENT_DELETE_SUCCESS.getMessage());
     }
 }
