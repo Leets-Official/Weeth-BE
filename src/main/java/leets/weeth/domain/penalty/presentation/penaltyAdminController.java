072b7f50811d440c89f20c3fdaa9ca64daa8b6ec
huncozyboy <leejh5838@naver.com>
2024-10-26 00:53:28 +0900

    refactor: penaltyAdminController 관련 성공 응답 수정

--- src/main/java/leets/weeth/domain/penalty/presentation/penaltyAdminController.java
+++ src/main/java/leets/weeth/domain/penalty/presentation/penaltyAdminController.java
@@ -1,15 +1,17 @@
 package leets.weeth.domain.penalty.presentation;
 
+import static leets.weeth.domain.penalty.presentation.ResponseMessage.PENALTY_ASSIGN_SUCCESS;
+import static leets.weeth.domain.penalty.presentation.ResponseMessage.PENALTY_DELETE_SUCCESS;
+import static leets.weeth.domain.penalty.presentation.ResponseMessage.PENALTY_FIND_ALL_SUCCESS;
+
 import leets.weeth.domain.penalty.application.dto.PenaltyDTO;
 import leets.weeth.domain.penalty.application.usecase.PenaltyUsecase;
-import leets.weeth.domain.penalty.domain.entity.enums.ResponseMessage;
+
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static leets.weeth.domain.penalty.domain.entity.enums.ResponseMessage.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,18 +23,21 @@ public class penaltyAdminController {
     @PostMapping
     public CommonResponse<String> assignPenalty(@RequestBody PenaltyDTO.Save dto){
         penaltyUsecase.save(dto);
-        return CommonResponse.createSuccess(PENALTY_ASSIGN_SUCCESS.getMessage());
+        return CommonResponse.createSuccess(PENALTY_ASSIGN_SUCCESS.getStatusCode(),
+                PENALTY_ASSIGN_SUCCESS.getMessage());
     }
 
     @GetMapping
     public CommonResponse<List<PenaltyDTO.Response>> findAll(){
-        return CommonResponse.createSuccess(penaltyUsecase.find());
+        return CommonResponse.createSuccess(PENALTY_FIND_ALL_SUCCESS.getStatusCode(),
+                PENALTY_FIND_ALL_SUCCESS.getMessage(),penaltyUsecase.find());
     }
 
     @DeleteMapping
     public CommonResponse<String> delete(@RequestParam Long penaltyId){
         penaltyUsecase.delete(penaltyId);
-        return CommonResponse.createSuccess(PENALTY_DELETE_SUCCESS.getMessage());
+        return CommonResponse.createSuccess(PENALTY_DELETE_SUCCESS.getStatusCode(),
+                PENALTY_DELETE_SUCCESS.getMessage());
     }
 
 }
