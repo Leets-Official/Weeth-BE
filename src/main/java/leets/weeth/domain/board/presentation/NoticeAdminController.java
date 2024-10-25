15ed3ed6faf575722b7a0495584cdb789fa4a1d6
huncozyboy <leejh5838@naver.com>
2024-10-26 02:32:09 +0900

    refactor: NoticeAdminController 관련 성공 응답 수정

--- src/main/java/leets/weeth/domain/board/presentation/NoticeAdminController.java
+++ src/main/java/leets/weeth/domain/board/presentation/NoticeAdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-import static leets.weeth.domain.board.domain.entity.enums.ResponseMessage.*;
+import static leets.weeth.domain.board.presentation.ResponseMessage.*;
 
 @RestController
 @RequiredArgsConstructor
