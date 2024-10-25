0b821ad5e743ea2e18be1715780641f40bbb21ef
huncozyboy <leejh5838@naver.com>
2024-10-26 02:38:09 +0900

    refactor: AttendanceController 관련 성공 응답 수정

--- src/main/java/leets/weeth/domain/attendance/presentation/AttendanceController.java
+++ src/main/java/leets/weeth/domain/attendance/presentation/AttendanceController.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import static leets.weeth.domain.attendance.application.dto.AttendanceDTO.*;
+import static leets.weeth.domain.attendance.presentation.ResponseMessage.ATTENDANCE_CHECKIN_SUCCESS;
+import static leets.weeth.domain.attendance.presentation.ResponseMessage.ATTENDANCE_FIND_ALL_SUCCESS;
+import static leets.weeth.domain.attendance.presentation.ResponseMessage.ATTENDANCE_FIND_SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,16 +23,16 @@ public class AttendanceController {
     @PatchMapping
     public CommonResponse<Void> checkIn(@CurrentUser Long userId, @RequestBody AttendanceDTO.CheckIn checkIn) throws AttendanceCodeMismatchException {
         attendanceUseCase.checkIn(userId, checkIn.code());
-        return CommonResponse.createSuccess();
+        return CommonResponse.createSuccess(ATTENDANCE_CHECKIN_SUCCESS.getMessage());
     }
 
     @GetMapping
     public CommonResponse<Main> find(@CurrentUser Long userId) {
-        return CommonResponse.createSuccess(attendanceUseCase.find(userId));
+        return CommonResponse.createSuccess(ATTENDANCE_FIND_SUCCESS.getMessage(), attendanceUseCase.find(userId));
     }
 
     @GetMapping("/detail")
     public CommonResponse<Detail> findAll(@CurrentUser Long userId) {
-        return CommonResponse.createSuccess(attendanceUseCase.findAll(userId));
+        return CommonResponse.createSuccess(ATTENDANCE_FIND_ALL_SUCCESS.getMessage(), attendanceUseCase.findAll(userId));
     }
 }
