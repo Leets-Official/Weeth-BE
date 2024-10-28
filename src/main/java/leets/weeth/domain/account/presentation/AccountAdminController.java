a3e83170efa82e5e48bbbde2174ca991214f3504
huncozyboy <leejh5838@naver.com>
2024-10-29 03:01:56 +0900

    refactor: Account 어드민 API들 Tag랑 operation 추가

--- src/main/java/leets/weeth/domain/account/presentation/AccountAdminController.java
+++ src/main/java/leets/weeth/domain/account/presentation/AccountAdminController.java
@@ -2,6 +2,8 @@ package leets.weeth.domain.account.presentation;
 
 import static leets.weeth.domain.account.presentation.ResponseMessage.ACCOUNT_SAVE_SUCCESS;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import leets.weeth.domain.account.application.dto.AccountDTO;
 import leets.weeth.domain.account.application.usecase.AccountUseCase;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "AdminController", description = "어드민 관련 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin/account")
@@ -20,6 +23,7 @@ public class AccountAdminController {
     private final AccountUseCase accountUseCase;
 
     @PostMapping
+    @Operation(summary="회비 총 금액 기입")
     public CommonResponse<Void> save(@RequestBody @Valid AccountDTO.Save dto) {
         accountUseCase.save(dto);
         return CommonResponse.createSuccess(ACCOUNT_SAVE_SUCCESS.getMessage());
