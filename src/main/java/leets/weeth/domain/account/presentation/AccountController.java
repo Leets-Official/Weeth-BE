cceddc7e9fd2f7b7aebbb0b75760ae5e9b4c9a09
huncozyboy <leejh5838@naver.com>
2024-10-29 02:55:54 +0900

    refactor: Admin API들 Tag랑 operation 추가

--- src/main/java/leets/weeth/domain/account/presentation/AccountController.java
+++ src/main/java/leets/weeth/domain/account/presentation/AccountController.java
@@ -2,6 +2,8 @@ package leets.weeth.domain.account.presentation;
 
 import static leets.weeth.domain.account.presentation.ResponseMessage.ACCOUNT_FIND_SUCCESS;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.weeth.domain.account.application.dto.AccountDTO;
 import leets.weeth.domain.account.application.usecase.AccountUseCase;
 import leets.weeth.global.common.response.CommonResponse;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Tag(name = "AccountController", description = "회비 관련 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/account")
@@ -19,6 +21,7 @@ public class AccountController {
     private final AccountUseCase accountUseCase;
 
     @GetMapping("/{cardinal}")
+    @Operation(summary="회비 내역 조회")
     public CommonResponse<AccountDTO.Response> find(@PathVariable Integer cardinal) {
         return CommonResponse.createSuccess(ACCOUNT_FIND_SUCCESS.getMessage(),accountUseCase.find(cardinal));
     }
--- src/main/java/leets/weeth/domain/schedule/presentation/MeetingController.java
+++ src/main/java/leets/weeth/domain/schedule/presentation/MeetingController.java
@@ -2,6 +2,8 @@ package leets.weeth.domain.schedule.presentation;
 
 import static leets.weeth.domain.schedule.presentation.ResponseMessage.MEETING_FIND_SUCCESS;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.weeth.domain.schedule.application.dto.MeetingDTO;
 import leets.weeth.domain.schedule.application.usecase.MeetingUseCase;
 import leets.weeth.global.common.response.CommonResponse;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "MeetingController", description = "정기모임 관련 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/meetings")
@@ -19,6 +22,7 @@ public class MeetingController {
     private final MeetingUseCase meetingUseCase;
 
     @GetMapping("/{meetingId}")
+    @Operation(summary="일정 상세 조회")
     public CommonResponse<MeetingDTO.Response> find(@PathVariable Long meetingId) {
         return CommonResponse.createSuccess(MEETING_FIND_SUCCESS.getMessage(),meetingUseCase.find(meetingId));
     }
