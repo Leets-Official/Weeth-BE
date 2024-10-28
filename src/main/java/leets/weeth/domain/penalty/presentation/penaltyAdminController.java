e8f473c2567f07be53d8333614b42f0459607767
huncozyboy <leejh5838@naver.com>
2024-10-29 02:59:35 +0900

    refactor: Penalty 어드민 API들 Tag랑 operation 추가

--- src/main/java/leets/weeth/domain/penalty/presentation/penaltyAdminController.java
+++ src/main/java/leets/weeth/domain/penalty/presentation/penaltyAdminController.java
@@ -4,6 +4,8 @@ import static leets.weeth.domain.penalty.presentation.ResponseMessage.PENALTY_AS
 import static leets.weeth.domain.penalty.presentation.ResponseMessage.PENALTY_DELETE_SUCCESS;
 import static leets.weeth.domain.penalty.presentation.ResponseMessage.PENALTY_FIND_ALL_SUCCESS;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.weeth.domain.penalty.application.dto.PenaltyDTO;
 import leets.weeth.domain.penalty.application.usecase.PenaltyUsecase;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "penaltyAdminController", description = "패널티 관련 어드민 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin/penalties")
@@ -21,17 +24,20 @@ public class penaltyAdminController {
     private final PenaltyUsecase penaltyUsecase;
 
     @PostMapping
+    @Operation(summary="전체 패널티 조회")
     public CommonResponse<String> assignPenalty(@RequestBody PenaltyDTO.Save dto){
         penaltyUsecase.save(dto);
         return CommonResponse.createSuccess(PENALTY_ASSIGN_SUCCESS.getMessage());
     }
 
     @GetMapping
+    @Operation(summary="패널티 부여")
     public CommonResponse<List<PenaltyDTO.Response>> findAll(){
         return CommonResponse.createSuccess(PENALTY_FIND_ALL_SUCCESS.getMessage(),penaltyUsecase.find());
     }
 
     @DeleteMapping
+    @Operation(summary="패널티 삭제")
     public CommonResponse<String> delete(@RequestParam Long penaltyId){
         penaltyUsecase.delete(penaltyId);
         return CommonResponse.createSuccess(PENALTY_DELETE_SUCCESS.getMessage());
