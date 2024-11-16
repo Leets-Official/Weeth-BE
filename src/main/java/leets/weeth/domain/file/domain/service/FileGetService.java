d5cb0d8d25f2b4a4670d4997c27166dfdca196ee
hyxklee <ewgt1234@naver.com>
2024-11-16 18:39:30 +0900

    feat: File 조회 서비스 추가

---
+++ src/main/java/leets/weeth/domain/file/domain/service/FileGetService.java
@@ -0,0 +1,23 @@
+package leets.weeth.domain.file.domain.service;
+
+import leets.weeth.domain.file.domain.entity.File;
+import leets.weeth.domain.file.domain.repository.FileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FileGetService {
+
+    private final FileRepository fileRepository;
+
+    public List<File> findAllByPost(Long postId) {
+        return fileRepository.findAllByPostId(postId);
+    }
+
+    public List<File> findAllByNotice(Long noticeId) {
+        return fileRepository.findAllByPostId(noticeId);
+    }
+}
