37151b22854dcb918a5b72fb45343de799b9a86a
hyxklee <ewgt1234@naver.com>
2024-11-16 18:39:46 +0900

    feat: notice 관련 로직 추가

--- src/main/java/leets/weeth/domain/file/application/mapper/FileMapper.java
+++ src/main/java/leets/weeth/domain/file/application/mapper/FileMapper.java
@@ -1,19 +1,26 @@
 package leets.weeth.domain.file.application.mapper;
 
+import leets.weeth.domain.board.domain.entity.Notice;
+import leets.weeth.domain.board.domain.entity.Post;
 import leets.weeth.domain.comment.application.mapper.CommentMapper;
-import leets.weeth.domain.file.application.dto.response.FileDto;
 import leets.weeth.domain.file.application.dto.response.FileResponse;
+import leets.weeth.domain.file.application.dto.response.UrlResponse;
 import leets.weeth.domain.file.domain.entity.File;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = CommentMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FileMapper {
 
-    File toFile(String fileName, String fileUrl);
+    @Mapping(target = "post", source = "post") // post 매핑
+    File toFile(String fileName, String fileUrl, Post post);
+
+    @Mapping(target = "notice", source = "notice") // post 매핑
+    File toFile(String fileName, String fileUrl, Notice notice);
 
     FileResponse toFileResponse(File file);
 
-    FileDto toFileDto(String putUrl, String getUrl);
+    UrlResponse toUrlResponse(String fileName, String putUrl);
 }
