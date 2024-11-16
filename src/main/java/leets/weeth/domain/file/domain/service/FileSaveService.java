e799f0e9bf683c44fe65762799adefeaa89f42e7
hyxklee <ewgt1234@naver.com>
2024-11-16 17:41:43 +0900

    feat: saveAll 메서드 구현

--- src/main/java/leets/weeth/domain/file/domain/service/FileSaveService.java
+++ src/main/java/leets/weeth/domain/file/domain/service/FileSaveService.java
@@ -5,12 +5,18 @@ import leets.weeth.domain.file.domain.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class FileSaveService {
     private final FileRepository fileRepository;
 
-    public void Save(File file) {
+    public void save(File file) {
         fileRepository.save(file);
+    }
+
+    public void save(List<File> files) {
+        fileRepository.saveAll(files);
     }
 }
