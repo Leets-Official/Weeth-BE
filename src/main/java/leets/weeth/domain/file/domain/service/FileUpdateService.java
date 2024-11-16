84975fe2944515878a27fb8ff11a0453b81ecabe
hyxklee <ewgt1234@naver.com>
2024-11-16 18:39:56 +0900

    feat: 파일 업데이트 로직 구현

---
+++ src/main/java/leets/weeth/domain/file/domain/service/FileUpdateService.java
@@ -0,0 +1,15 @@
+package leets.weeth.domain.file.domain.service;
+
+import leets.weeth.domain.file.application.dto.request.FileUpdateRequest;
+import leets.weeth.domain.file.domain.entity.File;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FileUpdateService {
+
+    public void update(File file, FileUpdateRequest dto) {
+        file.update(dto.fileName(), dto.fileUrl());
+    }
+}
