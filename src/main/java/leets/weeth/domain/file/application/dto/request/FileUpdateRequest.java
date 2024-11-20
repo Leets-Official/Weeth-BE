4f46cf39ab349c8f41663fb24fd942a310ebe9cf
hyxklee <ewgt1234@naver.com>
2024-11-20 21:40:46 +0900

    refactor: dto 유효성 검사 수정

--- src/main/java/leets/weeth/domain/file/application/dto/request/FileUpdateRequest.java
+++ src/main/java/leets/weeth/domain/file/application/dto/request/FileUpdateRequest.java
@@ -1,10 +1,11 @@
 package leets.weeth.domain.file.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.URL;
 
 public record FileUpdateRequest(
-        @NotBlank Long fileId,
+        @NotNull Long fileId,
         @NotBlank String fileName,
         @NotBlank @URL String fileUrl
 ) {
