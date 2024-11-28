1ac79fe6e6aaebf2d54b22879cd9e385a0feaebb
hyxklee <ewgt1234@naver.com>
2024-11-28 13:30:31 +0900

    refactor: 토큰에 사용자 역할 저장

--- src/main/java/leets/weeth/global/auth/jwt/service/JwtProvider.java
+++ src/main/java/leets/weeth/global/auth/jwt/service/JwtProvider.java
@@ -2,6 +2,7 @@ package leets.weeth.global.auth.jwt.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import leets.weeth.domain.user.domain.entity.enums.Role;
 import leets.weeth.global.auth.jwt.exception.InvalidTokenException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +18,7 @@ public class JwtProvider {
     private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
     private static final String EMAIL_CLAIM = "email";
     private static final String ID_CLAIM = "id";
+    private static final String ROLE_CLAIM = "role";
 
     @Value("${weeth.jwt.key}")
     private String key;
@@ -25,13 +27,14 @@ public class JwtProvider {
     @Value("${weeth.jwt.refresh.expiration}")
     private Long refreshTokenExpirationPeriod;
 
-    public String createAccessToken(Long id, String email) {
+    public String createAccessToken(Long id, String email, Role role) {
         Date now = new Date();
         return JWT.create()
                 .withSubject(ACCESS_TOKEN_SUBJECT)
                 .withExpiresAt(new Date(now.getTime() + accessTokenExpirationPeriod))
                 .withClaim(ID_CLAIM, id)
                 .withClaim(EMAIL_CLAIM, email)
+                .withClaim(ROLE_CLAIM, role.toString())
                 .sign(Algorithm.HMAC512(key));
     }
 
