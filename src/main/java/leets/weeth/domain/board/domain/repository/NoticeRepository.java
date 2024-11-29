9d651f76cb338f5022c7a590e2d670f7e49d11c3
jj <syeonie0526@gmail.com>
2024-11-29 19:00:41 +0900

    refactor: NoticeRepository의 공지사항 조회 방식을 slice를 이용하도록 변경

--- src/main/java/leets/weeth/domain/board/domain/repository/NoticeRepository.java
+++ src/main/java/leets/weeth/domain/board/domain/repository/NoticeRepository.java
@@ -1,21 +1,13 @@
 package leets.weeth.domain.board.domain.repository;
 
 import leets.weeth.domain.board.domain.entity.Notice;
-import leets.weeth.domain.board.domain.entity.Post;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
-    @Query("SELECT MAX(n.id) FROM Notice n")
-    Optional<Long>  findLastId();
-
-    @Query("SELECT n FROM Notice n WHERE n.id < :maxNoticeId ORDER BY n.id DESC")
-    List<Notice> findRecentNotices(@Param("maxNoticeId") Long maxNoticeId, Pageable pageable);
+    Slice<Notice> findPageBy(Pageable page);
 
 }
