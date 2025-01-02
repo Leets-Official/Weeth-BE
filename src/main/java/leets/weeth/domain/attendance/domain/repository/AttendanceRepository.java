973b06efc66beb99f814c6be40f8c797b4f2d748
hyxklee <ewgt1234@naver.com>
2025-01-02 14:11:51 +0900

    feat: 단일 쿼리로 삭제할 수 있도록 메서드 구현

--- src/main/java/leets/weeth/domain/attendance/domain/repository/AttendanceRepository.java
+++ src/main/java/leets/weeth/domain/attendance/domain/repository/AttendanceRepository.java
@@ -1,7 +1,18 @@
 package leets.weeth.domain.attendance.domain.repository;
 
 import leets.weeth.domain.attendance.domain.entity.Attendance;
+import leets.weeth.domain.schedule.domain.entity.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    List<Attendance> findAllByMeeting(Meeting meeting);
+
+    @Modifying
+    @Query("DELETE FROM Attendance a WHERE a IN :attendances")
+    void deleteAll(@Param("attendances") List<Attendance> attendances);
 }
