47defa0f755cbc49e2f05b51929b7c06e6c52169
huncozyboy <leejh5838@naver.com>
2025-01-09 04:59:18 +0900

    refactor : List로 입력받아 처리하도록 컨트롤러 수정

--- src/main/java/leets/weeth/domain/attendance/presentation/AttendanceAdminController.java
+++ src/main/java/leets/weeth/domain/attendance/presentation/AttendanceAdminController.java
@@ -6,13 +6,15 @@ import static leets.weeth.domain.attendance.presentation.ResponseMessage.ATTENDA
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import leets.weeth.domain.attendance.application.dto.AttendanceDTO.AttendanceInfo;
+import java.util.List;
+import leets.weeth.domain.attendance.application.dto.AttendanceDTO;
 import leets.weeth.domain.attendance.application.usecase.AttendanceUseCase;
 import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,16 +36,16 @@ public class AttendanceAdminController {
         return CommonResponse.createSuccess(ATTENDANCE_CLOSE_SUCCESS.getMessage());
     }
 
-    @GetMapping("/{attendanceId}")
-    @Operation(summary = "개별 출석 정보 조회")
-    public CommonResponse<AttendanceInfo> getAttendance(@PathVariable Long attendanceId) {
-        return CommonResponse.createSuccess(ATTENDANCE_FIND_DETAIL_SUCCESS.getMessage(), attendanceUseCase.findAttendance(attendanceId));
+    @GetMapping("/{meetingId}")
+    @Operation(summary = "모든 인원 정기모임 출석 정보 조회")
+    public CommonResponse<List<AttendanceDTO.AttendanceInfo>> getAllAttendance(@PathVariable Long meetingId) {
+        return CommonResponse.createSuccess(ATTENDANCE_FIND_DETAIL_SUCCESS.getMessage(), attendanceUseCase.findAllAttendanceByMeeting(meetingId));
     }
 
-    @PatchMapping("/{attendanceId}")
-    @Operation(summary = "출석 상태 수정")
-    public CommonResponse<Void> updateAttendanceStatus(@PathVariable Long attendanceId) {
-        attendanceUseCase.updateAttendanceStatus(attendanceId);
+    @PatchMapping("/status")
+    @Operation(summary = "모든 인원 정기모임 출석 상태 수정")
+    public CommonResponse<Void> updateAttendanceStatus(@RequestBody List<AttendanceDTO.UpdateStatus> attendanceUpdates) {
+        attendanceUseCase.updateAttendanceStatus(attendanceUpdates);
         return CommonResponse.createSuccess(ATTENDANCE_UPDATED_SUCCESS.getMessage());
     }
 }
