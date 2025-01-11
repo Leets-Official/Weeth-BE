d1614754951d3b05dd15a0572c9d475041ec29d8
hyxklee <ewgt1234@naver.com>
2025-01-11 21:57:11 +0900

    feat: 유저-기수 중간 테이블 저장소 구현

---
+++ src/main/java/leets/weeth/domain/user/domain/repository/UserCardinalRepository.java
@@ -0,0 +1,7 @@
+package leets.weeth.domain.user.domain.repository;
+
+import leets.weeth.domain.user.domain.entity.UserCardinal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCardinalRepository extends JpaRepository<UserCardinal, Long> {
+}
