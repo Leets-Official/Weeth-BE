f1b19b734e40974b9ccda0ce8da8928a5797d020
hyxklee <ewgt1234@naver.com>
2025-01-12 16:17:40 +0900

    feat: 기수 관련 매퍼 메서드 추가

--- src/main/java/leets/weeth/domain/user/application/mapper/CardinalMapper.java
+++ src/main/java/leets/weeth/domain/user/application/mapper/CardinalMapper.java
@@ -2,10 +2,15 @@ package leets.weeth.domain.user.application.mapper;
 
 import leets.weeth.domain.user.application.dto.request.CardinalSaveRequest;
 import leets.weeth.domain.user.application.dto.response.CardinalResponse;
+import leets.weeth.domain.user.application.dto.response.UserCardinalDto;
 import leets.weeth.domain.user.domain.entity.Cardinal;
+import leets.weeth.domain.user.domain.entity.User;
+import leets.weeth.domain.user.domain.entity.UserCardinal;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CardinalMapper {
@@ -13,4 +18,6 @@ public interface CardinalMapper {
     Cardinal from(CardinalSaveRequest dto);
 
     CardinalResponse to(Cardinal cardinal);
+
+    UserCardinalDto toUserCardinalDto(User user, List<UserCardinal> userCardinals);
 }
