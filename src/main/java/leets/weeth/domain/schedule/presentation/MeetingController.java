cf9a56d8d1baa9bcce15881e2b2d004c0c9f9ab8
hyxklee <ewgt1234@naver.com>
2025-01-31 18:01:17 +0900

    refactor: API 수정

--- src/main/java/leets/weeth/domain/schedule/presentation/MeetingController.java
+++ src/main/java/leets/weeth/domain/schedule/presentation/MeetingController.java
@@ -1,7 +1,5 @@
 package leets.weeth.domain.schedule.presentation;
 
-import static leets.weeth.domain.schedule.presentation.ResponseMessage.MEETING_FIND_SUCCESS;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.weeth.domain.schedule.application.dto.MeetingDTO;
@@ -13,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static leets.weeth.domain.schedule.presentation.ResponseMessage.MEETING_FIND_SUCCESS;
+
 @Tag(name = "MEETING", description = "정기모임 API")
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +22,7 @@ public class MeetingController {
     private final MeetingUseCase meetingUseCase;
 
     @GetMapping("/{meetingId}")
-    @Operation(summary="일정 상세 조회")
+    @Operation(summary="정기모임 상세 조회")
     public CommonResponse<MeetingDTO.Response> find(@PathVariable Long meetingId) {
         return CommonResponse.createSuccess(MEETING_FIND_SUCCESS.getMessage(),meetingUseCase.find(meetingId));
     }
