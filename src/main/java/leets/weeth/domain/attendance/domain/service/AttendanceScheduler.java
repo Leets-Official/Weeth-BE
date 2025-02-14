ec12fb23daf1bde70452a2887f093abf955c18b6
huncozyboy <leejh5838@naver.com>
2025-02-15 03:05:35 +0900

    refactor : MeetingStatus.OPEN인 출석객체들 목요일 10시마다 마감

--- src/main/java/leets/weeth/domain/attendance/domain/service/AttendanceScheduler.java
+++ src/main/java/leets/weeth/domain/attendance/domain/service/AttendanceScheduler.java
@@ -1,9 +1,9 @@
 package leets.weeth.domain.attendance.domain.service;
 
-import java.time.LocalDate;
 import java.util.List;
 import leets.weeth.domain.attendance.domain.entity.Attendance;
 import leets.weeth.domain.schedule.domain.entity.Meeting;
+import leets.weeth.domain.schedule.domain.entity.enums.MeetingStatus;
 import leets.weeth.domain.schedule.domain.repository.MeetingRepository;
 import leets.weeth.domain.user.domain.entity.Cardinal;
 import leets.weeth.domain.user.domain.repository.CardinalRepository;
@@ -22,16 +22,14 @@ public class AttendanceScheduler {
 
     @Scheduled(cron = "0 0 22 * * THU", zone = "Asia/Seoul")
     public void autoCloseAttendance() {
-        LocalDate today = LocalDate.now();
         Cardinal currentCardinal = cardinalRepository.findTopByOrderByCardinalNumberDesc();
 
         List<Meeting> meetings = meetingRepository.findAllByCardinalOrderByStartAsc(currentCardinal.getCardinalNumber());
 
         meetings.stream()
-                .filter(meeting -> meeting.getStart().toLocalDate().isEqual(today) &&
-                        meeting.getEnd().toLocalDate().isEqual(today))
-                .findAny()
-                .ifPresent(meeting -> {
+                .filter(meeting -> meeting.getMeetingStatus() == MeetingStatus.OPEN)
+                .forEach(meeting -> {
+                    meeting.close();
                     List<Attendance> attendanceList = attendanceGetService.findAllByMeeting(meeting);
                     attendanceUpdateService.close(attendanceList);
                 });
