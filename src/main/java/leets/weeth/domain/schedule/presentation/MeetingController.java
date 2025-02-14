26c73a0b9bac2177d386ed461a706182839bdd4a
hyxklee <ewgt1234@naver.com>
2025-02-14 13:05:19 +0900

    fix: 파라미터 무시

--- src/main/java/leets/weeth/domain/schedule/presentation/MeetingController.java
+++ src/main/java/leets/weeth/domain/schedule/presentation/MeetingController.java
@@ -1,6 +1,7 @@
 package leets.weeth.domain.schedule.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.weeth.domain.schedule.application.dto.MeetingDTO;
 import leets.weeth.domain.schedule.application.usecase.MeetingUseCase;
@@ -24,7 +25,7 @@ public class MeetingController {
 
     @GetMapping("/{meetingId}")
     @Operation(summary="정기모임 상세 조회")
-    public CommonResponse<MeetingDTO.Response> find(@CurrentUser Long userId,
+    public CommonResponse<MeetingDTO.Response> find(@Parameter(hidden = true) @CurrentUser Long userId,
                                                     @PathVariable Long meetingId) {
         return CommonResponse.createSuccess(MEETING_FIND_SUCCESS.getMessage(),meetingUseCase.find(userId, meetingId));
     }
