0b7a69418d86db269e6518945480d5990652d240
huncozyboy <leejh5838@naver.com>
2025-02-15 16:00:07 +0900

    refactor : MeetingStatus가 opne이면서 스케쥴러 가동시간 이전 시간만 조회하는 메서드 추가

--- src/main/java/leets/weeth/domain/schedule/domain/repository/MeetingRepository.java
+++ src/main/java/leets/weeth/domain/schedule/domain/repository/MeetingRepository.java
@@ -1,6 +1,7 @@
 package leets.weeth.domain.schedule.domain.repository;
 
 import leets.weeth.domain.schedule.domain.entity.Meeting;
+import leets.weeth.domain.schedule.domain.entity.enums.MeetingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -13,4 +14,6 @@ public interface MeetingRepository extends JpaRepository<Meeting, Long> {
     List<Meeting> findAllByCardinalOrderByStartAsc(int cardinal);
 
     List<Meeting> findAllByCardinal(int cardinal);
+
+    List<Meeting> findAllByMeetingStatusAndEndBeforeOrderByEndAsc(MeetingStatus status, LocalDateTime end);
 }
