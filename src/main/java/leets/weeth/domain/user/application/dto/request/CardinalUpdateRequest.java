b87abbaac27f0fe8d3225a1ef2ed059332d44512
hyxklee <ewgt1234@naver.com>
2025-02-18 15:24:53 +0900

    refactor: 기수는 수정하지 못하게 제거

--- src/main/java/leets/weeth/domain/user/application/dto/request/CardinalUpdateRequest.java
+++ src/main/java/leets/weeth/domain/user/application/dto/request/CardinalUpdateRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record CardinalUpdateRequest(
         @NotNull Long id,
-        @NotNull Integer cardinalNumber,
         @NotNull Integer year,
         @NotNull Integer semester,
         boolean inProgress
