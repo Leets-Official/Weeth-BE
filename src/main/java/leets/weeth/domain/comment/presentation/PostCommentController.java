ac6d8800f526dd5e9e1bac82edf65407c38f9797
huncozyboy <leejh5838@naver.com>
2025-03-10 17:32:33 +0900

    refactor : 게시판 댓글 컨트롤러 수정

--- src/main/java/leets/weeth/domain/comment/presentation/PostCommentController.java
+++ src/main/java/leets/weeth/domain/comment/presentation/PostCommentController.java
@@ -16,27 +16,27 @@ import leets.weeth.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "COMMENT-POST", description = "게시판 댓글 API")
+@Tag(name = "COMMENT-BOARD", description = "게시판 댓글 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/posts/{postId}/comments")
+@RequestMapping("/api/v1/boards/{boardId}/comments")
 public class PostCommentController {
 
     private final PostCommentUsecase postCommentUsecase;
 
     @PostMapping
     @Operation(summary="게시글 댓글 작성")
-    public CommonResponse<String> savePostComment(@RequestBody @Valid CommentDTO.Save dto, @PathVariable Long postId,
+    public CommonResponse<String> savePostComment(@RequestBody @Valid CommentDTO.Save dto, @PathVariable Long boardId,
                                                   @Parameter(hidden = true) @CurrentUser Long userId) {
-        postCommentUsecase.savePostComment(dto, postId, userId);
+        postCommentUsecase.savePostComment(dto, boardId, userId);
         return CommonResponse.createSuccess(POST_COMMENT_CREATED_SUCCESS.getMessage());
     }
 
     @PatchMapping("/{commentId}")
     @Operation(summary="게시글 댓글 수정")
-    public CommonResponse<String> updatePostComment(@RequestBody @Valid CommentDTO.Update dto, @PathVariable Long postId, @PathVariable Long commentId,
+    public CommonResponse<String> updatePostComment(@RequestBody @Valid CommentDTO.Update dto, @PathVariable Long boardId, @PathVariable Long commentId,
                                                     @Parameter(hidden = true) @CurrentUser Long userId) throws UserNotMatchException {
-        postCommentUsecase.updatePostComment(dto, postId, commentId, userId);
+        postCommentUsecase.updatePostComment(dto, boardId, commentId, userId);
         return CommonResponse.createSuccess(POST_COMMENT_UPDATED_SUCCESS.getMessage());
     }
 
