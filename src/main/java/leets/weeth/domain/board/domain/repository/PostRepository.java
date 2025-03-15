8912799a4c570df49b4ba80412e723585db76d14
jj <syeonie0526@gmail.com>
2025-03-15 22:21:29 +0900

    style: 들여쓰기 수정

--- src/main/java/leets/weeth/domain/board/domain/repository/PostRepository.java
+++ src/main/java/leets/weeth/domain/board/domain/repository/PostRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    Slice<Post> findPageBy(Pageable page);
+	Slice<Post> findPageBy(Pageable page);
 
 	Slice<Post> findByTitleContainingOrContentContainingIgnoreCase(String keyword1, String keyword2, Pageable pageable);
 }
