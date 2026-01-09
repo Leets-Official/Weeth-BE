8729de7f84035fd0adf569023a81b77bebbb327b
seokjun01 <suckjungod@naver.com>
2026-01-09 17:32:16 +0900

    fix : 의존성 변경에 의한 import 패키지 변경

--- src/test/java/leets/weeth/config/TestContainersTest.java
+++ src/test/java/leets/weeth/config/TestContainersTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.containers.MySQLContainer;
 
 @DataJpaTest
 @Import(TestContainersConfig.class)
