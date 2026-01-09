3a0550279c7c4448ded1819c3a0ccca6ab95d56c
seokjun01 <suckjungod@naver.com>
2026-01-09 18:09:38 +0900

    fix : gradle 동기화 롤백 커밋

--- src/test/java/leets/weeth/config/TestContainersConfig.java
+++ src/test/java/leets/weeth/config/TestContainersConfig.java
@@ -3,7 +3,7 @@ package leets.weeth.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.mysql.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
@@ -14,10 +14,7 @@ public class TestContainersConfig {
     @Bean
     @ServiceConnection
     public MySQLContainer mysqlContainer() {
-        MySQLContainer<?> container =
-            new MySQLContainer<>(DockerImageName.parse(MYSQL_IMAGE));
-
-        container.withReuse(true);
-        return container;
+        return new MySQLContainer(DockerImageName.parse(MYSQL_IMAGE))
+            .withReuse(true);
     }
 }
--- src/test/java/leets/weeth/config/TestContainersTest.java
+++ src/test/java/leets/weeth/config/TestContainersTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.mysql.MySQLContainer;
 
 @DataJpaTest
 @Import(TestContainersConfig.class)
